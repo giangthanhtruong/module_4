8b6eff4406097b78b09a866913b0d4389deeb1bc
giangthanhtruong <giangthanhtruong88@gmail.com>
2021-11-29 15:41:59 +0700

    ex5

---
+++ Component_Internation/angular-rating-bar/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RatingBarComponentComponent } from './rating-bar-component/rating-bar-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RatingBarComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
